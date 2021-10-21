8a2810d49d60fe056cab536b617c81daad8e7295
landeros31 <sebastianlanderos1@gmail.com>
2021-10-20 20:50:14 -0500

    init fronted

---
+++ fronted/intopcol/src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+function App() {
+  return (
+    
+    <Router>
+        <Switch>
+
+            <Route path='/login'>
+                <h1>estas en el login</h1>
+            </Route>
+
+           <Route path='/singup'>
+              <h1>esta en el registro</h1>
+            </Route>
+
+            <Route path='/chat'>
+              <h1>estas en el chat</h1>
+            </Route>
+
+            <Route path='/'>
+              <h1>estas en la pagina de inicio</h1>
+            </Route>
+        </Switch>
+    </Router>
+
+
+  );
+}
+
+export default App;
