2c522a7e8e191b34dcd30628e29c2857f78ab46d
landeros31 <sebastianlanderos1@gmail.com>
2021-10-22 11:27:08 -0500

    first version chat

---
+++ backend/models/server.js
@@ -0,0 +1,55 @@
+//Servidor express
+
+const express = require('express')
+const http=require('http')
+const socketio =require('socket.io')
+const path =require('path');
+const Sockets = require('./sockets');
+
+
+
+class Server{
+
+    constructor(){
+
+        this.app= express();
+        this.port=process.env.PORT;
+
+        //http server
+    
+        this.server = http.createServer(this.app)
+
+        //configuracion socket
+        //configuracion socket
+        this.io = socketio(this.server,{/* configuraciones*/ });
+       
+    }
+
+    middlewares(){
+
+        //desplegar directorio publico
+
+        this.app.use(express.static(path.resolve(__dirname,'../public') )); 
+    }
+
+    configurarSockets(){
+
+        new Sockets(this.io);
+    }
+
+    execute(){
+
+        //inicializar middlewares
+        this.middlewares();
+
+        //inicializar sockets
+        this.configurarSockets();
+
+        //inicializar server
+        this.server.listen(this.port,()=>{
+            console.log("servidor corriendo en puerto ",this.port)
+        });
+    }
+}
+
+module.exports=Server;
