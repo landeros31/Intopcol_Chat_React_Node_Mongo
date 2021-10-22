d84e0e74f9a358961c533c1927d5be26764cae41
landeros31 <sebastianlanderos1@gmail.com>
2021-10-22 15:42:42 -0500

    full duplex works

--- backend/models/server.js
+++ backend/models/server.js
@@ -5,6 +5,7 @@ const http=require('http')
 const socketio =require('socket.io')
 const path =require('path');
 const Sockets = require('./sockets');
+const cors =require('cors')
 
 
 
@@ -30,6 +31,7 @@ class Server{
         //desplegar directorio publico
 
         this.app.use(express.static(path.resolve(__dirname,'../public') )); 
+        this.app.use(cors())
     }
 
     configurarSockets(){
